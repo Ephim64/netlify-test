fdc72fc4ae894995cf8fdecfed78c65011861ad6
Efim Gotsko <gotsco-en@yandex.ru>
2020-05-27 20:59:21 +0400

    Setup simple redirect

--- src/api.js
+++ src/api.js
@@ -4,10 +4,45 @@ const serverless = require('serverless-http');
 const app = express();
 const router = express.Router();
 
+const rootPrefix = '/.netlify/functions/api';
+
+let query = null;
+let srcUrl = null;
+
 router.get('/', (req, res) => {
   res.json({hello: 'hi'});
 });
 
-app.use('/.netlify/functions/api', router);
+router.get('/start', (req, res) => {
+  query = '?' + Object.keys(req.query).map(key => `${key}=${req.query[key]}`).join('&');
+  srcUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+
+  res.redirect(`${rootPrefix}/end`);
+});
+
+router.get('/end', (req, res) => {
+  const htmlString = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta property="og:title" content="Title" />
+      <meta property="og:url" content="${srcUrl}" />
+      <meta property="og:description" content="You came with this query: ${query}" />
+      <meta property="og:title" content="Title" />
+      <title>Test site</title>
+      <meta
+    </head>
+    <body>
+      <h1>Query?</h1>
+      <div>Here: ${query}</div>
+    </body>
+  </html>
+  `;
+
+  res.set('Content-Type', 'text/html');
+  res.send(htmlString);
+})
+
+app.use(rootPrefix, router);
 
 module.exports.handler = serverless(app);
